4fe91a394edf6b87a7c15b0bf5d9e86981506777
Jake Neal <hello@jakeneal.co.uk>
2023-05-17 19:27:25 +0100

    :wheelchair: added lang attribute

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,6 +21,9 @@ export default defineNuxtConfig({
     app: {
         head: {
             title,
+            htmlAttrs: {
+                lang: 'en',
+            },
             meta: [
                 {
                     name: 'description',
