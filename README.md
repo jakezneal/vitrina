fa6aa1e408819d5d43387429587f20bf82c59ad0
Jake Neal <hello@jakeneal.co.uk>
2023-05-19 15:55:38 +0100

    :mag: added og/twitter meta

--- README.md
+++ README.md
@@ -10,10 +10,11 @@
 - [ ] tidy up scss files
 - [ ] removed duplicated `rem` function from sfc
 - [ ] add 'skip to content link'
-- [ ] add schema data and open graph meta
+- [-] add schema data and open graph meta
 - [-] add 'skills' page
 - [ ] add 'bounce' animation to arrow on recent work section
 - [ ] add `@nuxt/devtools`
 - [ ] get dynamic titles working on work case study pages
 - [ ] fix `@nuxt/image`
 - [ ] add favicon
+- [ ] refactor to use typescript
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-const title = 'Jake Neal - Freelance Front-End Engineer';
+const meta = {
+    title: 'Jake Neal - Freelance Front-End Engineer',
+    description: 'Freelance Front-End Engineer based in Manchester, UK',
+    url: 'jakeneal.co.uk',
+    image: '',
+};
+
+const socialShareImage = `https://${ meta.url }/images/social-share.png`;
+
+meta.image = socialShareImage;
 
 export default defineNuxtConfig({
     srcDir: './src',
@@ -14,20 +23,19 @@ export default defineNuxtConfig({
 
     runtimeConfig: {
         public: {
-            appTitle: title,
+            appTitle: meta.title,
         }
     },
 
     app: {
         head: {
-            title,
             htmlAttrs: {
                 lang: 'en',
             },
             meta: [
                 {
                     name: 'description',
-                    content: 'Freelance Front-End Engineer based in Manchester, UK',
+                    content: meta.description,
                 },
                 {
                     name: 'keywords',
@@ -39,9 +47,59 @@ export default defineNuxtConfig({
                         'front end',
                         'engineer',
                         'developer',
+                        'web developer',
                         'Manchester',
+                        'Congleton',
+                        'Cheshire',
+                        'UK',
+                        'freelance web developer',
+                        'contract web developer',
                     ].join(', '),
-                }
+                },
+                {
+                    property: 'og:url',
+                    content: `https://${ meta.url }`,
+                },
+                {
+                    property: 'og:type',
+                    content: 'website',
+                },
+                {
+                    property: 'title',
+                    content: meta.title,
+                },
+                {
+                    property: 'description',
+                    content: meta.description,
+                },
+                {
+                    property: 'og:image',
+                    content: meta.image,
+                },
+                {
+                    name: 'twitter:card',
+                    content: 'summary_large_image',
+                },
+                {
+                    property: 'twitter:domain',
+                    content: meta.url,
+                },
+                {
+                    property: 'twitter:url',
+                    content: `https://${ meta.url }`,
+                },
+                {
+                    name: 'twitter:title',
+                    content: meta.title,
+                },
+                {
+                    name: 'twitter:description',
+                    content: meta.description,
+                },
+                {
+                    name: 'twitter:image',
+                    content: meta.image,
+                },
             ],
         },
     }
